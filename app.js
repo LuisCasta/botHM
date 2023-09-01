61565be5ae78b74f018b03f05737e0ebea2e87a2
LuisCasta <luis.alankasta@hotmail.com>
2023-09-01 12:58:59 -0600

    test send whatsapp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios');
 
 const app = express()
 const routes = require('./router/index')
@@ -11,6 +12,27 @@ const HOST = "0.0.0.0"//'https://intestinolimpio.ferringcloud1a.com/backend';
 app.get('/node', function (req, res) {
     res.send('Bienvenidos hmApp')
 })
+
+app.get('/node/test', async function (req, res) {
+
+    const resultado2 = await axios({
+        method: 'post',
+        url: "https://messages.landbot.io/wa/W-1818-YLSJCP6KP6OF6WQ0/opt_in?phone=+528714353747",//'https://api.landbot.io/v1/customers/297652403/send_template/',
+        //responseType: 'json',
+        headers: {
+            'Authorization': 'Token 5046a0bf7add2c578e59ac8713fff7fe8300a589',
+            'Content-Type' : 'application/json'
+        }
+    });
+
+    if(resultado2.status == 200){
+        res.send({estatus:"success"})
+    }else{
+        res.send({estatus:"error"})
+    }
+    
+})
+
 
 
 app.use(bodyParser.urlencoded({extended:true}));
